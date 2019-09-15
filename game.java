2c234b4e5093e2a977c6c4205489b54b904bd970
Cloverfieldo <cloverfieldor@gmail.com>
2019-09-15 17:06:30 -0300

    arquivo java

---
+++ game.java
@@ -0,0 +1,13 @@
+public class OlaMundo {
+    public static void main(String[] args){
+        System.out.println("Ola mundo");
+    }
+}
+
+/*
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+    }
+}
+*/
