a7b274390d453a1e0a84466209e209d8068647d3
Cloverfieldo <cloverfieldor@gmail.com>
2019-09-14 21:15:03 -0300

    coloquei o resto dos arquivos

---
+++ arquivo.py
@@ -0,0 +1,11 @@
+class Animal:
+    def shit(self):
+        return 'coco'
+
+class Dog(Animal):
+    def bark(self):
+        return 'au-au'
+
+class Cat(Animal):
+    def meow(self):
+        return 'miau'
