539aad9fb695699d9f07e4cd3f860c943ba431b2
Cloverfieldo <cloverfieldor@gmail.com>
2019-09-17 23:21:29 -0300

    Jogo de adivinhação em python

--- arquivo.py
+++ arquivo.py
@@ -1,11 +1,7 @@
-class Animal:
-    def shit(self):
-        return 'coco'
-
-class Dog(Animal):
-    def bark(self):
-        return 'au-au'
-
-class Cat(Animal):
-    def meow(self):
-        return 'miau'
+import random
+randomNumber = random.randint(0, 10)
+inputOfUser = input("Eu gerei um numero aleatório, advinhe qual é ")
+if inputOfUser == randomNumber:
+    print(f"Você acertou")
+else:
+    print(f"Você errou o numero é {randomNumber}")
