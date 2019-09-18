45d938ed84fdb770072bc00697b971ab1627214f
Cloverfieldo <cloverfieldor@gmail.com>
2019-09-18 12:13:45 -0300

    dicas adicionadas

--- arquivo.py
+++ arquivo.py
@@ -1,6 +1,9 @@
 import random
 randomNumber = random.randint(0, 10)
-inputOfUser = input("Eu gerei um numero aleatório, advinhe qual é ")
+randomPlus = random.randint(1, 5)
+randomLess = random.randint(1, 5)
+print(f"Esse numero é menor que {randomNumber + randomPlus} e é maior que {randomNumber - randomLess}")
+inputOfUser = int(input("Eu gerei um numero aleatório, advinhe qual é \n "))
 if inputOfUser == randomNumber:
     print(f"Você acertou")
 else:
