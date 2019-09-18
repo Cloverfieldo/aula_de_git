498f3c7a26291a30217eedc9837195a34ebe2219
Cloverfieldo <cloverfieldor@gmail.com>
2019-09-18 12:14:50 -0300

    classes de um possível jogo

--- game.java
+++ game.java
@@ -1,13 +1,52 @@
-public class OlaMundo {
-    public static void main(String[] args){
-        System.out.println("Ola mundo");
-    }
+public class enemy{
+
+}
+public class playable{
+
+}
+public class human extends playable {
+
+}
+public class mage extends human{
+
+}
+public class warrior extends human{
+
+}
+public class archer extends human{
+
+}
+public class orc{
+
+}
+public class superOrc extends enemy{
+
+} 
+public class minionOrc extends enemy{
+
 }
 
-/*
-public class OlaMundo {
-    public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
-    }
+//cenario
+public class world{
+
 }
-*/
+public class worldOfIce extends world{
+
+}
+public class worldOfFire extends world{
+
+}
+
+//habilidade
+public class skills{
+
+}
+public class archerSkills extends skills {
+
+}
+public class warriorSkills extends skills{
+
+}
+public class mageSkills extends skills{
+    
+}
